0cb13cc1eb784b62a1fe31e6407cb5981bd808ef
Abaan5467 <mohammad5467@students.dpssgr.com>
2024-08-07 22:25:19 +0530

    REMOVED INDEX.HTML FROM FRONTEND FOLDER TO TEMPLATES FOLDER

--- main.py
+++ main.py
@@ -12,9 +12,16 @@ def convert_currency(amount, from_currency, to_currency):
     converted_amount = base_rate * amount * data['data'][to_currency]
     return converted_amount
 
-@app.route('/<amount>/<from_currency>/<to_currency>')
-def home(amount, from_currency, to_currency):
-    return f'{amount}{from_currency} = {convert_currency(float(amount), from_currency.upper(), to_currency.upper())} {to_currency}'
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+    # return f'{amount}{from_currency} = {convert_currency(float(amount), from_currency.upper(), to_currency.upper())} {to_currency}'
+@app.route('/convert', methods=['POST'])
+def convert():
+    amount = request.form.get('amount')
+    from_currency = request.form.get('fromCurrency')
+    to_currency = request.form.get('toCurrency')
+    return render_template('index.html',output=f'{convert_currency(float(amount), from_currency.upper(), to_currency.upper())} {to_currency}')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port='8000',debug=True)
