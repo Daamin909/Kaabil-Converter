7a6b2b844cf41ae5e637f729ab7b99634846d6d6
Abaan5467 <mohammad5467@students.dpssgr.com>
2024-08-07 19:51:04 +0530

    updated main.py

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 def convert_currency(amount, from_currency, to_currency):
-    url = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_ZuGAAxXLvgIDdbeAWacrOONoWbvAKbtEh0JhSDZU"
+    url = f"https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_ZuGAAxXLvgIDdbeAWacrOONoWbvAKbtEh0JhSDZU&currencies={from_currency}%2C{to_currency}%2CUSD"
     response = requests.get(url)
     data = response.json()
+    print(data)
     base_rate = data['data']['USD']/data['data'][from_currency]
     converted_amount = base_rate * amount * data['data'][to_currency]
     return converted_amount
 
 @app.route('/<amount>/<from_currency>/<to_currency>')
 def home(amount, from_currency, to_currency):
-    return f'{convert_currency(float(amount), from_currency, to_currency)}'
+    return f'{amount}{from_currency} = {convert_currency(float(amount), from_currency.upper(), to_currency.upper())} {to_currency}'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port='8000',debug=True)
