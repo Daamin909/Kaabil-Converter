e9a7010e4b44df8fa55f51abee6c1fe32903ac08
Abaan5467 <mohammad5467@students.dpssgr.com>
2024-08-07 19:29:41 +0530

    updated main.py

--- main.py
+++ main.py
@@ -3,15 +3,10 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-
-def get_data():
+def convert_currency(amount, from_currency, to_currency):
     url = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_ZuGAAxXLvgIDdbeAWacrOONoWbvAKbtEh0JhSDZU"
     response = requests.get(url)
     data = response.json()
-    return data
-
-def convert_currency(amount, from_currency, to_currency):
-    data = get_data()
     base_rate = data['data']['USD']/data['data'][from_currency]
     converted_amount = base_rate * amount * data['data'][to_currency]
     return converted_amount
