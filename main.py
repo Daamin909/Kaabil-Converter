c70b1990017e9cec02a37a777fc36c1a9aa284ca
Daamin909 <ashai.daamin@gmail.com>
2024-08-08 15:20:44 +0530

    made css and bacground UI changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from flask import Flask, request, jsonify, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 def convert_currency(amount, from_currency, to_currency):
     url = f"https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_ZuGAAxXLvgIDdbeAWacrOONoWbvAKbtEh0JhSDZU&currencies={from_currency}%2C{to_currency}%2CUSD"
@@ -15,13 +15,12 @@ def convert_currency(amount, from_currency, to_currency):
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
-    # return f'{amount}{from_currency} = {convert_currency(float(amount), from_currency.upper(), to_currency.upper())} {to_currency}'
 @app.route('/convert', methods=['POST'])
 def convert():
     amount = request.form.get('amount')
     from_currency = request.form.get('fromCurrency')
     to_currency = request.form.get('toCurrency')
-    return render_template('index.html',output=f'{convert_currency(float(amount), from_currency.upper(), to_currency.upper())} {to_currency}')
+    return render_template('index.html',output=f'{amount} {from_currency.upper()} = {round(convert_currency(float(amount), from_currency.upper(), to_currency.upper()), 4)} {to_currency}')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port='8000',debug=True)
